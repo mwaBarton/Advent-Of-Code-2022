8a996df733967e5af012a9cd72ab58edf7c54ded
matwx <matwx@DESKTOP-3H6AKT9>
2022-12-01 18:24:36 +0000

    Add project files.

---
+++ Day 01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("input.txt");
+
+            List<int> elfCals = new List<int>();
+
+            int cals = 0;
+            foreach (string line in input)
+            {
+                if (line == "")
+                {
+                    elfCals.Add(cals);
+                    cals = 0;
+                }
+                else
+                {
+                    cals += int.Parse(line);
+                }
+            }
+            elfCals.Add(cals);
+
+            elfCals.Sort();
+
+            Console.WriteLine(elfCals[elfCals.Count - 1] + elfCals[elfCals.Count - 2] + elfCals[elfCals.Count - 3]);
+        }
+    }
+}
