ef2d0a570770c0c881133b9fef962a6a9d8896c4
Matthew Arnmor <MWA@barton.ac.uk>
2022-12-01 18:32:19 +0000

    Getting Day 2 ready

---
+++ Day 02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Day_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("Input.txt");
+
+            Console.ReadKey();
+        }
+    }
+}
