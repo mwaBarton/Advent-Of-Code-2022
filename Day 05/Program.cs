b87c1f0c86bf54417cfd04ffbe5134f5b696712a
Matthew Arnmor <MWA@barton.ac.uk>
2022-12-06 07:19:21 +0000

    Day 5 part 1

---
+++ Day 05/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Day_05
+{
+    internal class Program
+    {
+        static string prettyCrateLine(string[] crateLine)
+        {
+            string result = "";
+
+            for(int i = 0; i < crateLine.Length; i++)
+            {
+                result += crateLine[i];
+                if (i < crateLine.Length - 1)
+                {
+                    result += ", ";
+                }
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            const int numStacks = 9;
+
+            // Get two lists of the two parts of the input
+            List<string[]> inputCrates = new List<string[]>();
+            List<string> inputCommands = new List<string>();
+
+            using(StreamReader sr = new StreamReader("input.txt"))
+            {
+                string line = sr.ReadLine();
+                do
+                {
+                    string[] temp = new string[numStacks];
+                    for (int i = 0; i < temp.Length; i++)
+                    {
+                        int index = i * 4 + 1;
+                        if (line.Length >= index + 1)
+                        {
+                            if (line[index] == ' ')
+                            {
+                                temp[i] = "";
+                            } else
+                            {
+                                temp[i] = line[index].ToString();
+                            }
+                        } else
+                        {
+                            temp[i] = "";
+                        }
+                    }
+                    inputCrates.Add(temp);
+
+                    line = sr.ReadLine();
+
+                } while (line[1] != '1');
+
+                sr.ReadLine();
+
+                while (!sr.EndOfStream)
+                {
+                    line = sr.ReadLine().Replace("move ", "").Replace("from ", "").Replace("to ", "");
+                    inputCommands.Add(line);
+                }
+            }
+
+            // Load the stacks
+            List<Stack<string>> stacks = new List<Stack<string>>();
+            for (int i = 0; i < numStacks; i++)
+            {
+                stacks.Add(new Stack<string>());
+
+                for (int j = inputCrates.Count - 1; j >= 0; j--)
+                {
+                    if (inputCrates[j][i] != "") stacks[i].Push(inputCrates[j][i]);
+                }
+            }
+
+            // Print the inputs
+            Console.WriteLine(inputCrates.Select((a) => { return prettyCrateLine(a); }).Aggregate((a, b) => { return a + "\n" + b; }));
+            Console.WriteLine(inputCommands.Aggregate((a, b) => { return a + "\n" + b; }));
+
+            // Load the commands
+            foreach (string c in inputCommands)
+            {
+                var parts = c.Split(' ');
+
+                for (int i = 0; i < int.Parse(parts[0]); i++)
+                {
+                    stacks[int.Parse(parts[2]) - 1].Push(stacks[int.Parse(parts[1]) - 1].Pop());
+                }
+            }
+
+            Console.WriteLine();
+            foreach (var stack in stacks)
+            {
+                Console.WriteLine(prettyCrateLine(stack.ToArray()));
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
