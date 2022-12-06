128954648d7f38c8905df0bf711016583573b686
Matthew Arnmor <MWA@barton.ac.uk>
2022-12-06 18:47:33 +0000

    Day 6 complete

---
+++ Day 06/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Day_06
+{
+    internal class Program
+    {
+        static bool AreAllCharactersDifferent(string s)
+        {
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = 0; j < s.Length; j++)
+                {
+                    if (i != j && s[i] == s[j]) return false;
+                }
+            }
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            string input = File.ReadAllText("input.txt");
+
+
+            int startOfPacket = 0;
+            for (int i = 13; i < input.Length; i++)
+            {
+                string sub = input.Substring(i - 13, 14);
+                //Console.WriteLine(sub);
+                if (AreAllCharactersDifferent(sub))
+                {
+                    startOfPacket = i + 1;
+                    Console.WriteLine(startOfPacket);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
